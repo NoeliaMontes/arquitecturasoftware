8b04a66ddda861b7ae59bbfdb47df7074add1b07
NoeliaMontes <nmontessa.inf@upsa.es>
2026-03-05 13:18:16 +0100

    Intento de merge
    Se corrompió el github

---
+++ practica3/app_gestion_taller/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import lista_clientes#, detalle_cliente,
+from .views import registrar_cliente, registrar_coche, registrar_servicio 
+from .views import buscar_cliente, buscar_coche_por_matricula, buscar_coches_de_cliente
+
+urlpatterns = [
+    path('clientes/', lista_clientes, name='lista_clientes'),
+    ##path('clientes/<int:cliente_id>/', detalle_cliente, name='detalle_cliente'),
+    path('clientes/registrar/', registrar_cliente, name='registrar_cliente'),
+    path('coches/registrar/', registrar_coche, name='registrar_coche'),
+    path('servicios/registrar/', registrar_servicio, name='registrar_servicio'),
+    path('clientes/<int:cliente_id>/', buscar_cliente, name='buscar_cliente'),
+    path('coches/matricula/<str:matricula>/', buscar_coche_por_matricula, name='buscar_coche_por_matricula'),
+    path('clientes/<int:cliente_id>/coches/', buscar_coches_de_cliente, name='buscar_coches_de_cliente'),
+]
